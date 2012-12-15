50a7386760d384cc54a065c3ba6f3c21bf5d3952
kawuum <siegfried.depner@googlemail.com>
2012-12-15 16:34:07 +0100

    b

---
+++ ergebnisse_bundesebene.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include("head.php");?>
+<body>
+
+	<div id='jqxDockPanel' style="background: none; border: none;">	
+					
+		<div dock='top'>
+			<?php include("menu_bundesebene.html"); ?>		
+		</div>
+		<div dock='left'>
+			<?php include("menu.html") ?>
+		</div>
+		<div id='Sitzverteilung_Sitzverteilung' dock='right'>
+			Bundesebene
+			<div id='pieChartBundesebene' style="width: 680px; height: 400px;"></div>
+		</div>
+	</div>
+	
+	
+</body>
+</html>
