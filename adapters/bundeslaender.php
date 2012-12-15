2d80d965882b284cfc92817f373a68ab26588204
kawuum <siegfried.depner@googlemail.com>
2012-12-15 19:39:33 +0100

    changed json bundselaender data format

--- adapters/bundeslaender.php
+++ adapters/bundeslaender.php
@@ -2,7 +2,7 @@
 
 $conn = pg_connect("host=link port=5432 dbname=btw2009 user=btw2009 password=btw2009") or die("Connection error...");
 
-$qresult = pg_query( $conn, "SELECT id, '-1' as partentid, name FROM land WHERE jahr='2009'") or die("Query error...");
+$qresult = pg_query( $conn, "SELECT id, '-1' as partentid, name as text FROM land WHERE jahr='2009'") or die("Query error...");
 
 
 $result = array();
