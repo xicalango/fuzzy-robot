855b9fd501becef152ce9e4dc696f91cf4afce6d
kawuum <siegfried.depner@googlemail.com>
2012-12-15 16:59:40 +0100

    and back again...

--- adapters/sitzverteilung_diag_daten.php
+++ adapters/sitzverteilung_diag_daten.php
@@ -2,10 +2,7 @@
 
 $conn = pg_connect("host=link port=5432 dbname=btw2009 user=btw2009 password=btw2009") or die("Connection error...");
 
-$qresult = pg_query( $conn, "SELECT p.name, count(*) AS sitze
-							FROM wahl_gewinner_rischtisch, partei p
-							WHERE p.id = wahl_gewinner_rischtisch.partei_id
-							GROUP BY wahl_gewinner_rischtisch.partei_id, p.name") or die("Query error...");
+$qresult = pg_query( $conn, "SELECT * FROM sitzverteilung_diagramm") or die("Query error...");
 
 
 $result = array();
