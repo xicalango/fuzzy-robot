1f739ee7a42e9d1a34525f7e7d91be1720eafb5d
kawuum <siegfried.depner@googlemail.com>
2012-12-16 20:05:54 +0100

    changed output format

--- adapters/top_10_knappste.php
+++ adapters/top_10_knappste.php
@@ -5,6 +5,11 @@
 $partei_id = $_GET["parteiid"];
 
 $da = new DataAdapter();
-$da->evalQueryToJSON( "(SELECT * from get_10_knappste_sieger($1)) union (Select * from get_10_knappste_verlierer($1))", [$partei_id] );
+$da->evalQueryToJSON( "(
+SELECT ks.vorname, ks.nachname, p.name as partei from get_10_knappste_sieger($1) ks, partei p 
+where p.id = ks.partei_id
+union 
+SELECT ks.vorname, ks.nachname, p.name from get_10_knappste_verlierer($1) ks, partei p 
+where p.id = ks.partei_id)", [ partei_id] );
 
 ?>
