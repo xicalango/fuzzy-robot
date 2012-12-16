e57a9e974feec6d83e0fb04dd223ed7eac962d6d
kawuum <siegfried.depner@googlemail.com>
2012-12-16 19:13:34 +0100

    changed header

--- mitglieder_bundestag_list.php
+++ mitglieder_bundestag_list.php
@@ -1,27 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title id='Description'>This example shows how to create a Grid from Array data.</title>
-    <link rel="stylesheet" href="../../jqwidgets/styles/jqx.base.css" type="text/css" />
-    <script type="text/javascript" src="../../scripts/jquery-1.6.2.min.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxcore.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxdata.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxbuttons.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxscrollbar.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxmenu.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxgrid.js"></script>
-    <script type="text/javascript" src="../../jqwidgets/jqxgrid.selection.js"></script>
-    <script type="text/javascript">
+   <title id='Description'>Mitglieder des Bundestags</title>
+    <link rel="stylesheet" href="lib/jqwidgets/styles/jqx.base.css" type="text/css" />
+    <script type="text/javascript" src="lib/scripts/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxdata.js"></script>    
+	<script type="text/javascript" src="lib/jqwidgets/jqxscrollbar.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxmenu.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxgrid.js"></script>
+    <script type="text/javascript" src="lib/jqwidgets/jqxgrid.selection.js"></script>
+	<script type="text/javascript">
         $(document).ready(function () {
             // prepare the data
             
 			
-			
-			
-			
-
-
-var source =
+		var source =
 			{
 				 datatype: "json",
 				 datafields: [
