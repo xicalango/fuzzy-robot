540441d1a5418e32bdc0d05ccc2609a1c6ff738e
kawuum <siegfried.depner@googlemail.com>
2012-12-16 18:40:12 +0100

    wrong parameters

--- adapters/wahlkreissieger_partei_erststimme.php
+++ adapters/wahlkreissieger_partei_erststimme.php
@@ -2,9 +2,7 @@
 
 @include("data_adapter.php");
 
-$wahlkreis_id = $_GET["wahlkreisid"];
-
 $da = new DataAdapter();
-$da->evalQueryToJSON( "SELECT * FROM wahlkreissieger_partei_erststimme($1)", [$wahlkreis_id] );
+$da->evalQueryToJSON( "SELECT * FROM wahlkreissieger_partei_erststimme");
 
 ?>
--- adapters/wahlkreissieger_partei_zweitstimme.php
+++ adapters/wahlkreissieger_partei_zweitstimme.php
@@ -2,9 +2,8 @@
 
 @include("data_adapter.php");
 
-$wahlkreis_id = $_GET["wahlkreisid"];
 
 $da = new DataAdapter();
-$da->evalQueryToJSON( "SELECT * FROM wahlkreissieger_partei_zweitstimme($1)", [$wahlkreis_id] );
+$da->evalQueryToJSON( "SELECT * FROM wahlkreissieger_partei_zweitstimme");
 
 ?>
