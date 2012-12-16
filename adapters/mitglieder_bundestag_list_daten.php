12a0958d2eef934ebbd4b6695de7973b13ccbf65
kawuum <siegfried.depner@googlemail.com>
2012-12-16 04:28:20 +0100

    removed menu-bars (they did not properly working in this version) and added some adapters

---
+++ adapters/mitglieder_bundestag_list_daten.php
@@ -0,0 +1,18 @@
+<?
+
+$conn = pg_connect("host=link port=5432 dbname=btw2009 user=btw2009 password=btw2009") or die("Connection error...");
+
+$qresult = pg_query( $conn, "SELECT * from mitglieder_bundestag") or die("Query error...");
+
+$result = array();
+
+while($arr = pg_fetch_assoc($qresult)) {
+	array_push($result, $arr);
+}
+
+
+
+header('Content-type: application/json');
+echo json_encode($result);
+
+?>
--- header.php
+++
@@ -1,8 +0,0 @@
-	<link rel="stylesheet" href="lib/jqwidgets/styles/jqx.base.css" type="text/css" />
-    <script type="text/javascript" src="lib/scripts/gettheme.js"></script>
-    <script type="text/javascript" src="lib/scripts/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="lib/jqwidgets/jqxcore.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxdata.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxchart.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxmenu.js"></script>
-	<script type="text/javascript" src="lib/jqwidgets/jqxdockpanel.js"></script>
